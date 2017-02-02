997256aa3786e43e08d0ded7ec4d5e739e84c700
jasonl12 <jasonliang812@gmail.com>
2017-02-01 17:27:58 -0800

    Add files via upload

---
+++ virtualbox/bootstrap.sh
@@ -0,0 +1,11 @@
+sed -i 's/mesg n/tty -s \&\& mesg n/g' /root/.profile
+
+apt-get update -y > /dev/null 2>&1
+apt-get upgrade -y > /dev/null 2>&1
+apt-get install -y vim zsh git chrony > /dev/null 2>&1
+
+systemctl stop exim4.service
+systemctl stop nfs-common.service
+systemctl disable exim4.service > /dev/null 2>&1
+systemctl disable nfs-common.service > /dev/null 2>&1
+timedatectl set-timezone America/Los_Angeles
---
+++ virtualbox/increase_swap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# size of swapfile in megabyte
+swapsize=512
+grep -q "swapfile" /etc/fstab
+
+if [ $? -ne 0 ]; then
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile > /dev/null 2>&1
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+  echo 'swapfile found. No changes made.'
+fi
